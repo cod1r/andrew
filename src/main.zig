46cd4fc5ad09264b8758cefac969e3e65ae11108
cod1r <jasonho353@gmail.com>
2022-09-27 00:42:11 -0500

    we can accept connections

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.log.info("All your codebase are belong to us.", .{});
+    var add = try std.net.Address.parseIp("127.0.0.1", 8000);
+    var ss = std.net.StreamServer.init(.{});
+    defer ss.deinit();
+    try ss.listen(add);
+    var conn = try ss.accept();
+    var buff: [50000]u8 = undefined;
+    var sent_size = try conn.stream.write("HTTP/2.0 200 \r\n");
+    _ = sent_size;
+    var read_size = try conn.stream.read(buff[0..]);
+    std.debug.print("{s}\n", .{buff[0..read_size]});
 }
