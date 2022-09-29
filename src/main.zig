ca0c2f3239cfa426c778422a2174f09dcb69053b
cod1r <jasonho353@gmail.com>
2022-09-29 12:34:31 -0500

    Fixed msg body being read with wrong lengths. Added a json string validator condition. Added a function that converts hex string to actual hex bytes. Added tests.

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,7 @@ fn parse_http_message(buff: []u8, map: *std.StringHashMap([]u8)) !usize {
         try map.put("start_line", buff[0..]);
     }
     var index: usize = newline_start_line + 1;
+    var read: usize = newline_start_line + 1;
     while (index < buff.len) {
         if (index + 3 < buff.len and std.mem.eql(u8, buff[index..(index + 4)], "\r\n\r\n")) {
             break;
@@ -51,6 +52,7 @@ fn parse_http_message(buff: []u8, map: *std.StringHashMap([]u8)) !usize {
 
             try map.put(key, value);
             index = newline_value_end + 1;
+            read += (newline_value_end + 1 - read);
         } else if (key_end > index and newline_value_end <= key_end + 1) {
             var key = try alloc.alloc(u8, key_end - index);
             std.mem.copy(u8, key, buff[index..key_end]);
@@ -62,21 +64,22 @@ fn parse_http_message(buff: []u8, map: *std.StringHashMap([]u8)) !usize {
 
             try map.put(key, value);
             index = buff.len;
+            read += (buff.len - read);
         } else {
             break;
         }
     }
-    return index;
+    return read;
 }
 
 // Acknowledging the PING request from Discord
 fn ACK(stream: std.net.Stream) !usize {
-    return try stream.write("HTTP/1.1 200 \t\r\nContent-Type: application/json\r\n\r\n'{\"type\": 1}'");
+    return try stream.write("HTTP/1.1 200 \t\r\nContent-Length: 13\r\nContent-Type: application/json\r\n\r\n'{\"type\": 1}'");
 }
 
 // Bad Signature
 fn BAD_SIG(stream: std.net.Stream) !usize {
-    return try stream.write("HTTP/1.1 401 \t\r\n");
+    return try stream.write("HTTP/1.1 401 \t\r\nContent-Length: 25\r\nContent-Type: text/plain\r\n\r\ninvalid request signature");
 }
 
 var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
@@ -90,35 +93,45 @@ pub fn main() !void {
     try ss.listen(add);
     std.debug.print("listening on port {}\n", .{PORT});
     if (libsodium.sodium_init() < 0) {
-        unreachable;
+        @panic("libsodium cannot be initialized\n");
     }
     std.debug.print("libsodium initialized\n", .{});
     while (true) {
         var conn = try ss.accept();
         var buff: [65536]u8 = undefined;
         var read_size = try conn.stream.read(buff[0..]);
-        std.debug.print("{s}\n", .{buff[0..read_size]});
+        var msg = buff[0..read_size];
         var map = std.StringHashMap([]u8).init(alloc);
         defer map.deinit();
-        var msg_size = try parse_http_message(buff[0..read_size], &map);
+        var msg_size = try parse_http_message(msg, &map);
         var signature = map.get("X-Signature-Ed25519");
         var timestamp = map.get("X-Signature-Timestamp");
         if (signature != null and timestamp != null) {
             // adding 2 because of the CRLF empty line
-            std.debug.print("signature: {s}\n", .{signature.?});
-            std.debug.print("timestamp: {s}\n", .{timestamp.?});
+            var body = msg[msg_size + 2 .. read_size];
+            // TODO: We need to consider the case that we don't get the full HTTP Message and Body
+            if (!std.json.validate(body)) {
+                @panic("NOT VALID JSON STRING");
+            }
             var timestamp_body = try std.mem.concat(
                 alloc,
                 u8,
-                &[_][]u8{ timestamp.?, buff[msg_size + 2 ..] },
+                &[_][]u8{ timestamp.?, body },
             );
-            std.debug.print("TIMESTAMP_BODY: {s}\n", .{timestamp_body});
+            defer alloc.free(timestamp_body);
+
+            var sig_hex = try utils.fromHex(alloc, signature.?);
+            defer alloc.free(sig_hex);
+
+            var public_key_hex = try utils.fromHex(alloc, env_vars.PUBLIC_KEY);
+            defer alloc.free(public_key_hex);
+
             // verify request with libsodium
             var verify = libsodium.crypto_sign_verify_detached(
+                @ptrCast([*c]const u8, sig_hex.ptr),
                 @ptrCast([*c]const u8, timestamp_body.ptr),
-                @ptrCast([*c]const u8, signature.?),
-                signature.?.len,
-                @ptrCast([*c]const u8, env_vars.PUBLIC_KEY),
+                @intCast(c_ulonglong, timestamp_body.len),
+                @ptrCast([*c]const u8, public_key_hex.ptr),
             );
             std.debug.print("VERIFY STATUS: {}\n", .{verify});
             if (verify == -1) {
@@ -150,7 +163,3 @@ test "env public key" {
     try std.testing.expect(env_vars.PUBLIC_KEY.len > 0);
 }
 
-test "libsodium" {
-    var sodium_init_res = libsodium.sodium_init();
-    try std.testing.expect(sodium_init_res != -1);
-}
--- src/utils.zig
+++ src/utils.zig
@@ -7,8 +7,190 @@ pub fn check_newline_carriage_return(in: []u8) void {
         }
     }
 }
+const HexConvertErr = error{InvalidHexString};
+pub fn fromHex(alloc: std.mem.Allocator, hexStr: []const u8) ![]u8 {
+    // 2 because hex string shows each byte two characters
+    var res = try alloc.alloc(u8, hexStr.len / 2);
+    for (res) |*loc| {
+        loc.* = 0;
+    }
+    var idx_hexStr: usize = 0;
+    var idx_res: usize = 0;
+    while (idx_hexStr + 1 < hexStr.len) {
+        var firstbyte: u8 = hexStr[idx_hexStr];
+        var secondbyte: u8 = hexStr[idx_hexStr + 1];
+        if (firstbyte >= '0' and firstbyte <= '9') {
+            res[idx_res] |= (firstbyte - '0') << 4;
+        } else if (firstbyte >= 'a' and firstbyte <= 'f') {
+            res[idx_res] |= (10 + (firstbyte - 'a')) << 4;
+        } else {
+            return HexConvertErr.InvalidHexString;
+        }
+        if (secondbyte >= '0' and secondbyte <= '9') {
+            res[idx_res] |= (secondbyte - '0');
+        } else if (secondbyte >= 'a' and secondbyte <= 'f') {
+            res[idx_res] |= (10 + (secondbyte - 'a'));
+        } else {
+            return HexConvertErr.InvalidHexString;
+        }
+        idx_hexStr += 2;
+        idx_res += 1;
+    }
+    return res;
+}
 
-pub fn fromHex(alloc: std.mem.Allocator, hexStr: []u8) ![]u8 {
-    var res = alloc.alloc(u8, hexStr.len / 4);
-    _ = res;
+test "fromHex: signature" {
+    var slice = try fromHex(
+        std.testing.allocator,
+        "d86906c229e9682014db13e29bb9d213739f95a3bdd3f58e08ff45ac468f27a836b34396b71b98b2b64df3092f6b2f527da0570772e955c83d4d95aeaa0a360e",
+    );
+    defer std.testing.allocator.free(slice);
+    var correct = [_]u8{
+        216,
+        105,
+        6,
+        194,
+        41,
+        233,
+        104,
+        32,
+        20,
+        219,
+        19,
+        226,
+        155,
+        185,
+        210,
+        19,
+        115,
+        159,
+        149,
+        163,
+        189,
+        211,
+        245,
+        142,
+        8,
+        255,
+        69,
+        172,
+        70,
+        143,
+        39,
+        168,
+        54,
+        179,
+        67,
+        150,
+        183,
+        27,
+        152,
+        178,
+        182,
+        77,
+        243,
+        9,
+        47,
+        107,
+        47,
+        82,
+        125,
+        160,
+        87,
+        7,
+        114,
+        233,
+        85,
+        200,
+        61,
+        77,
+        149,
+        174,
+        170,
+        10,
+        54,
+        14,
+    };
+    try std.testing.expect(slice.len == std.mem.len(&correct));
+    for (slice) |byte, idx| {
+        try std.testing.expect(byte == correct[idx]);
+    }
+}
+
+test "fromHex: signature2" {
+    var slice = try fromHex(
+        std.testing.allocator,
+        "876bdbe52418099d2de1793eb30877302de79f905f0129ce7b096793e6ca544f66b2c03d9b5e5d7d1b68dd3bc4a214be99bc4a117bdfecb1b39f464e53ab830f",
+    );
+    defer std.testing.allocator.free(slice);
+    var correct = [_]u8{
+        135,
+        107,
+        219,
+        229,
+        36,
+        24,
+        9,
+        157,
+        45,
+        225,
+        121,
+        62,
+        179,
+        8,
+        119,
+        48,
+        45,
+        231,
+        159,
+        144,
+        95,
+        1,
+        41,
+        206,
+        123,
+        9,
+        103,
+        147,
+        230,
+        202,
+        84,
+        79,
+        102,
+        178,
+        192,
+        61,
+        155,
+        94,
+        93,
+        125,
+        27,
+        104,
+        221,
+        59,
+        196,
+        162,
+        20,
+        190,
+        153,
+        188,
+        74,
+        17,
+        123,
+        223,
+        236,
+        177,
+        179,
+        159,
+        70,
+        78,
+        83,
+        171,
+        131,
+        15,
+    };
+    try std.testing.expect(std.mem.len(correct) == slice.len);
+    for (correct) |byte, idx| {
+        try std.testing.expect(byte == slice[idx]);
+    }
 }
